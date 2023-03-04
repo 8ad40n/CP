cda5660057dd51e2597585a16be305b94ada2ec5
Badhon Nath Joy <8ad40n@gmail.com>
2023-03-04 21:48:16 +0600

    .

---
+++ CODE/Code Forces/Area.cpp
@@ -0,0 +1,30 @@
+//Template
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl        "\n"
+#define pb          push_back
+#define pop         pop_back
+#define FOR         for(int i=0;i<n;i++)
+#define yes         cout<<"YES"<<endl;
+#define no          cout<<"NO"<<endl;
+#define BOOST       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pi          3.141592653
+int main()
+{
+    BOOST;
+
+    //const pi=3.141592653;
+
+    cout<<fixed;
+    cout<<setprecision(9);
+
+    double r;
+    cin>>r;
+    double ans= pi*(r*r);
+
+    
+
+    cout<<ans<<endl;
+
+}
