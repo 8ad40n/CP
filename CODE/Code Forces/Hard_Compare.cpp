d2c29ea3a835e81e891bc037f8b6c0af696fbbe7
Badhon Nath Joy <8ad40n@gmail.com>
2023-03-05 20:25:03 +0600

    added

---
+++ CODE/Code Forces/Hard_Compare.cpp
@@ -0,0 +1,25 @@
+//Template
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl        "\n"
+#define pb          push_back
+#define pop         pop_back
+#define FOR         for(int i=0;i<n;i++)
+#define yes         cout<<"YES"<<endl;
+#define no          cout<<"NO"<<endl;
+#define BOOST       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pi          3.141592653589793238
+
+int main()
+{
+    BOOST
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (pow(a, b) > pow(c, d)) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
