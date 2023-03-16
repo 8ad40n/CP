5a1a9831c037e41c753b18717bad041d611c947e
Badhon Nath Joy <8ad40n@gmail.com>
2023-03-17 01:45:43 +0600

    added

---
+++ CODE/Code Forces/Basic_Data_Types.cpp
@@ -0,0 +1,32 @@
+//Template
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl        "\n"
+#define pb          push_back
+#define pop         pop_back
+#define FOR         for(int i=0;i<n;i++)
+#define yes         cout<<"YES"<<endl;
+#define no          cout<<"NO"<<endl;
+#define BOOST       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pi          3.141592653589793238
+
+int main()
+{
+    BOOST;
+
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    cin>> a>>b>>c>>d>>e;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+
+}
